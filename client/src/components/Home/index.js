30c996783f1be9ed21015d4eb660e9d8b0575c46
michaelsheng15 <m3sheng@uwaterloo.ca>
2022-06-17 18:48:21 -0400

    Deliverable commit

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -6,10 +6,19 @@ import { MuiThemeProvider, createTheme } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Paper from "@material-ui/core/Paper";
+import MenuItem from '@material-ui/core/MenuItem';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
 
 
 //Dev mode
-const serverURL = ""; //enable for dev mode
+const serverURL = "http://ov-research-4.uwaterloo.ca:3055"; //enable for dev mode
 
 //Deployment mode instructions
 //const serverURL = "http://ov-research-4.uwaterloo.ca:PORT"; //enable for deployed mode; Change PORT to the port number given to you;
@@ -68,17 +77,21 @@ const styles = theme => ({
 });
 
 
+
+
+
+
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userID: 1,
-      mode: 0
+      mode: 0,
     }
   };
 
   componentDidMount() {
-    //this.loadUserSettings();
+    this.loadUserSettings();
   }
 
 
@@ -111,40 +124,195 @@ class Home extends Component {
     return body;
   }
 
+
+
+
   render() {
     const { classes } = this.props;
 
+    const Review = () => {
+
+      const [selectedMovie, setSelectedMovie] = React.useState('');
+      const [enteredTitle, setEnteredTitle] = React.useState('');
+      const [enteredReview, setEnteredReview] = React.useState('');
+      const [selectedRating, setSelectedRating] = React.useState('');
+      const [displayMessage, setDisplayMessage] = React.useState('');
+
+      const handleTitleChange = (e) => {
+        console.log(e.target.value)
+        setEnteredTitle(e.target.value);
+      }
+      const handleMovieChange = (e) => {
+        console.log(e.target.value)
+        setSelectedMovie(e.target.value);
+      }
+      const handleBodyChange = (e) => {
+        console.log(e.target.value)
+        setEnteredReview(e.target.value);
+      }
+      const handleScoreChange = (e) => {
+        console.log(e.target.value)
+        setSelectedRating(e.target.value);
+      }
+
+      const handleSubmit = (e) => {
+        console.log(selectedMovie)
+        console.log(enteredTitle)
+        console.log(enteredReview)
+        console.log(selectedRating)
+
+        if(enteredTitle == ""){
+          setDisplayMessage("Please enter your review title");
+        }else if (enteredReview == ""){
+          setDisplayMessage("Please enter your review");
+        }else if (selectedRating == ""){
+          setDisplayMessage("Please select the rating")
+        }else{
+          setDisplayMessage(selectedMovie + "\n" + enteredTitle + "\n" + enteredReview + "\n" + selectedRating + "\n");
+        }
+      }
+
+
+
+      return (
+        <Grid
+          container
+          spacing={8}
+          direction="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          style={{ minHeight: '100vh' }}
+          className={classes.mainMessageContainer}
+        >
+          <Grid item>
+            <Typography variant="h3">
+              Review a movie!
+            </Typography>
+          </Grid>
+
+          <MovieSelection onChange={handleMovieChange} />
+          <ReviewTitle onChange={handleTitleChange} />
+          <ReviewBody onChange={handleBodyChange} />
+          <ReviewRating onChange={handleScoreChange} />
+
+          <Grid item>
+            <Button variant="contained" color="primary" onClick={handleSubmit}>
+              Submit
+            </Button>
+          </Grid>
+          <Grid>
+            {displayMessage}
+          </Grid>
+
+
+        </Grid>
+      )
+    }
+
+    const MovieSelection = (props) => {
+      return (
+        <Grid item>
+          <InputLabel>Select a Movie</InputLabel>
+          <Select onChange={props.onChange}>
+            <MenuItem value={"Jurassic World Dominion"}>Jurassic World Dominion</MenuItem>
+            <MenuItem value={"Lightyear"}>Lightyear</MenuItem>
+            <MenuItem value={"Top Gun: Maverick"}>Top Gun: Maverick</MenuItem>
+            <MenuItem value={"Doctor Strange in the Multiverse of Madness"}>Doctor Strange in the Multiverse of Madness</MenuItem>
+            <MenuItem value={"Sonic the Hedgehog 2"}>Sonic the Hedgehog 2</MenuItem>
+          </Select>
+        </Grid>
+      )
+    }
+
+    const ReviewTitle = (props) => {
+      return (
+        <Grid item>
+          <TextField id="standard-basic" label="Title" onChange={props.onChange} />
+        </Grid>
+      )
+    }
+
+    const ReviewBody = (props) => {
+      return (
+        <Grid item>
+          <TextField
+            label="Enter your review here:"
+            multiline
+            inputProps={{ maxLength: 200 }}
+            variant="outlined"
+            onChange={props.onChange}
+          />
+        </Grid>
+      )
+    }
+
+    const ReviewRating = (props) => {
+      return (
+        <Grid item>
+          <RadioGroup row aria-label="position" name="position" defaultValue="top" onChange={props.onChange}>
+            <Radio
+              value="1"
+              label="1"
+              labelPlacement="top"
+              inputProps={{ 'aria-label': '1' }}
+            />
+            <Radio
+              value="2"
+              label="2"
+              labelPlacement="top"
+              inputProps={{ 'aria-label': '2' }}
+            />
+            <Radio
+              value="3"
+              label="3"
+              labelPlacement="top"
+              inputProps={{ 'aria-label': '3' }}
+            />
+            <Radio
+              value="4"
+              label="4"
+              labelPlacement="top"
+
+              inputProps={{ 'aria-label': '4' }}
+            />
+            <Radio
+              value="5"
+              label="5"
+              labelPlacement="top"
+
+              inputProps={{ 'aria-label': '5' }}
+            />
+          </RadioGroup>
+        </Grid>
+      )
+    }
 
 
     const mainMessage = (
       <Grid
         container
-        spacing={0}
+        spacing={8}
         direction="column"
-        justify="flex-start"
+        justifyContent="flex-start"
         alignItems="flex-start"
         style={{ minHeight: '100vh' }}
         className={classes.mainMessageContainer}
       >
-        <Grid item>
-
-          <Typography
-            variant={"h3"}
-            className={classes.mainMessage}
-            align="flex-start"
-          >
-            {this.state.mode === 0 ? (
-              <React.Fragment>
-                Welcome to MSci245!
-              </React.Fragment>
-            ) : (
-              <React.Fragment>
-                Welcome back!
-              </React.Fragment>
-            )}
-          </Typography>
-
-        </Grid>
+        <Typography
+          variant={"h3"}
+          className={classes.mainMessage}
+          align="flex-start"
+        >
+          {this.state.mode === 0 ? (
+            <React.Fragment>
+              Welcome to MSCI245
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              Welcome back!
+            </React.Fragment>
+          )}
+        </Typography>
       </Grid>
     )
 
@@ -157,8 +325,9 @@ class Home extends Component {
             className={classes.paper}
           >
             {mainMessage}
-          </Paper>
+            <Review />
 
+          </Paper>
         </div>
       </MuiThemeProvider>
     );
